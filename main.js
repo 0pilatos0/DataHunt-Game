3f1d28affb15c56b5ec928e9dfdd343e79b476bd
Paul <paulvanderlei@hotmail.nl>
2021-08-31 14:47:36 +0200

    added html loader

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import Canvas from './Core/Canvas.js';
+import HtmlLoader from './Core/Loaders/HtmlLoader.js';
 
 let canvas = new Canvas();
+let htmlLoader = new HtmlLoader();
+
 
 setInterval(() => {
     canvas.Update();
