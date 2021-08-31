fdc0bf2d5f668ed176fdaa2507f048e37a174d8f
Paul <Paulvanderlei@hotmail.nl>
2021-08-31 20:35:52 +0200

    added menu

--- loadingscreen/script.js
+++
@@ -1,7 +0,0 @@
-//on window load fadeout the loading screen
-window.onload = function() {
-    //get loading screen
-    var loadScreen = document.getElementById("loading");
-    loadScreen.classList.add("loaderdisabled");
-
-}
