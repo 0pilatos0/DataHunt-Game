91968780680378d1568a311a228a65c4adea0eb6
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-09-01 19:46:35 +0200

    added fps counter

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ import Canvas from './Core/Canvas.js';
 
 let canvas = new Canvas();
 
+let deltaTime = 1 / 60
+let startTime = Date.now()
+let fps = 60
+
 let loop = () => {
     canvas.Update();
     
     canvas.Draw();
     
+    deltaTime = (Date.now() - startTime) / 1000
+    fps = 1 / deltaTime
+    startTime = Date.now()
+
+    canvas.ctx.font = '48px serif';
+    canvas.ctx.fillStyle = '#fff'
+    canvas.ctx.fillText(fps.toFixed(2), 10, 50)
+
     window.requestAnimationFrame(loop);
 }
 
