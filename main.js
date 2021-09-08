8d0b6bc942e8b0252db63396dc09db9dd567cf6d
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-09-08 09:14:35 +0200

    optimized canvas styling

--- main.js
+++ main.js
@@ -60,3 +60,13 @@ let loop = () => {
 }
 
 window.requestAnimationFrame(loop);
+
+let client = io('localhost:3000', {'reconnection': true, 'reconnectionDelay': 1000, 'reconnectionDelayMax': 2000})
+
+client.on('connect', () => {
+    console.log("connected to server")
+})
+
+client.on('disconnect', () => {
+    console.log('disconnected from server')
+})
