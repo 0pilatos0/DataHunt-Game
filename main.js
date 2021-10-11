5f014b40298e6dc4c93fc0d85d53af70f063c019
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-10-11 10:26:58 +0200

    fixed bug showing loader till connected to server

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ function runAfterLoad(){
     //TODO fix bug with account page which requires client
     //TODO load client at this point
     // window.MainMenu.Show()
-    window.LoadingScreen.Hide()
+    // window.LoadingScreen.Hide()
 }
 
 // window.player = new Player();
