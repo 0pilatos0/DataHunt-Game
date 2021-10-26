764cd33884fa8d7aa0eeadbfc6a8de38cabec59e
0pilatos0 <Paulvanderlei@hotmail.nl>
2021-10-26 12:55:36 +0200

    forgot to remove comment

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ window.client = io('datahunt.duckdns.org:3000', {'reconnection': true, 'reconnec
 window.client.on('connect', () => {
     console.log("connected to server")
     //TODO dont forget to comment this out
-    // window.LoadingScreen.Hide()
-    // window.AccountMenu.Show()
+    window.LoadingScreen.Hide()
+    window.AccountMenu.Show()
 })
 
 window.client.on('disconnect', () => {
