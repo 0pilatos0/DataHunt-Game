26d4a0d53d741d35bebd8e361fed6350aa6ea07a
0pilatos0 <Paulvanderlei@hotmail.nl>
2021-10-26 10:51:24 +0200

    added version 1 of the tutorial

---
+++ Tutorial/Tutorial.js
@@ -0,0 +1,73 @@
+import HtmlLoader from "../Core/Loaders/HtmlLoader.js";
+import Menu from "../Core/Menu.js";
+
+export default class Tutorial extends Menu{
+
+    constructor(){
+        super();
+        this.menu = document.querySelector('#tutorial');
+        this.menucontext = HtmlLoader.Load('./assets/elements/TutorialWindow.html');
+        this.menucontext.then(data =>{
+            let context = data;
+            let script = context.split('<script>')[1].split('</script>')[0];
+            let html = context.split('<script>')[0];
+
+            this.menu.innerHTML = html;
+
+            eval(script);
+
+            this.Hide()
+            
+            this.Trigger('ready')
+        })
+    }
+    
+    Start(){
+        this.menu.style.display = 'block';
+        this.steps = this.menu.children;
+        this.currentStep = 0;
+
+        this.steps[0].style.display = 'block';
+
+        this.keys = ['w', 'a', 's', 'd'];
+        this.correctPressed = [];
+
+        document.addEventListener('keydown', (e) => {
+            if(this.keys.includes(e.key)){
+                this.correctPressed.push(e.key);
+                this.keys = this.keys.filter(key => key !== e.key);
+
+            } 
+            console.log(this.correctPressed);
+            console.log(this.keys.length);
+            if(this.keys.length === 0){
+                if(this.currentStep == 0){
+                    this.steps[0].style.display = 'none';
+                    this.steps[1].style.display = 'block';
+                    this.keys = ["i"]
+                    this.correctPressed = [];
+                    this.currentStep++;
+                }
+                else if(this.currentStep == 1){
+                    this.steps[1].style.display = 'none';
+                    this.steps[2].style.display = 'block';
+                    this.keys = ["c"]
+                    this.correctPressed = [];
+                    this.currentStep++;
+                }
+                else if(this.currentStep == 2){
+                    this.steps[2].style.display = 'none';
+                    this.Complete();
+                }
+            }
+        })   
+    }
+    
+    Complete(){
+        this.menu.style.display = 'none';
+        this.currentStep = 0;
+
+        localStorage.setItem('tutorialcompleted', true);
+    }
+
+}
