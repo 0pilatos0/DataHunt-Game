061bef44ef63cbc33cde332b5698b1a800c4828b
Sjaak Kant <kantsjaak@gmail.com>
2021-11-01 11:40:31 +0100

    fixed type + removed useless function

--- Inventory/inventory.js
+++ Inventory/inventory.js
@@ -4,8 +4,8 @@ export default class Inventory extends Event {
 
 
     /**
-     * @param {int} id
-     * @return {object} inventory data
+     * @param {int} id characterId
+     * @return {object} inventorydata
      */
     get(id) {
         window.client.emit('getInventory', id)
@@ -13,16 +13,6 @@ export default class Inventory extends Event {
             Object.entries(data).map(entry=>{
                 console.log(entry);
             })
-            // Object.entries(data).map(entry=>{
-            //     console.log(entry);
-            // })
-            // this.createItem("DIV", "inventoryWindow", 'inventoryGearItems');
-            // Object.entries(data).map(entry => {
-            //     console.log(`${JSON.stringify(entry[1])}`)
-            //     console.log(entry[1][name])
-            //     this.createItem("DIV", `inventoryGearItems`, `inventory${entry[0]}`);
-            //     this.createItem("IMG", `inventory${entry[0]}`, `${entry[0]}Image`, null, null);
-            //     this.createItem("P", `inventory${entry[0]}`, `${entry[0]}Text`, `${entry[1].name}`);
-            // })
         })
     }
+}
