056e454a4c3d4102c98aa3a1d13f47cb6c3cad65
Sjaak Kant <kantsjaak@gmail.com>
2021-11-01 11:38:06 +0100

    small update to inventory js

--- Inventory/inventory.js
+++ Inventory/inventory.js
@@ -1,4 +1,28 @@
-window.client.emit('getItem', 'request data')
-window.client.on('item', (data)=>{
-    console.log(data);
-})
+import Event from "../Core/Event.js";
+
+export default class Inventory extends Event {
+
+
+    /**
+     * @param {int} id
+     * @return {object} inventory data
+     */
+    get(id) {
+        window.client.emit('getInventory', id)
+        window.client.on('returnedInventory', (data) => {
+            Object.entries(data).map(entry=>{
+                console.log(entry);
+            })
+            // Object.entries(data).map(entry=>{
+            //     console.log(entry);
+            // })
+            // this.createItem("DIV", "inventoryWindow", 'inventoryGearItems');
+            // Object.entries(data).map(entry => {
+            //     console.log(`${JSON.stringify(entry[1])}`)
+            //     console.log(entry[1][name])
+            //     this.createItem("DIV", `inventoryGearItems`, `inventory${entry[0]}`);
+            //     this.createItem("IMG", `inventory${entry[0]}`, `${entry[0]}Image`, null, null);
+            //     this.createItem("P", `inventory${entry[0]}`, `${entry[0]}Text`, `${entry[1].name}`);
+            // })
+        })
+    }
