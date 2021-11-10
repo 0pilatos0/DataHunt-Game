f07998364c67b2f1ef0eb9cc08a807073886d8ba
Sjaak Kant <kantsjaak@gmail.com>
2021-11-10 19:49:18 +0100

    Elementcreator done

--- Inventory/inventory.js
+++ Inventory/inventory.js
@@ -2,15 +2,19 @@ import Event from "../Core/Event.js";
 import Create from "../Core/ElementCreator.js";
 
 export default class Inventory extends Event {
-
-
+    constructor() {
+        super();
+        console.log('inventory')
+    }
     /**
      * @param {int} id characterId
      * @return {object} inventorydata
      */
-    get(id) {
+    Get(id) {
         window.client.emit('getInventory', id)
         window.client.on('returnedInventory', (data) => {
+            console.log('data')
+            // console.log(`inventory keybind is ${window.KeyBindManager.GetKeybindByAction('inventory')}`)
             Object.entries(data).map(entry => {
                 entry[1].map(item => {
                     if (item.index == 0) {
@@ -46,20 +50,21 @@ export default class Inventory extends Event {
                             },
                             `inventory${entry[0]}`,
                             null,
-                            [
-                                {
-                                    element: null,
-                                    key: null,
-                                    type: 'click',
-                                    function: this.changeItem
-                                },
-                                {
-                                    element: document,
-                                    key: 'i',
-                                    type: 'keydown',
-                                    function: this.StopchangeItem
-                                }
-                            ]
+                            null
+                            // [
+                            //     {
+                            //         element: null,
+                            //         key: null,
+                            //         type: 'click',
+                            //         function: this.changeItem
+                            //     },
+                            //     {
+                            //         element: document,
+                            //         key: window.KeyBindManager.GetKeybindByAction('inventory'),
+                            //         type: 'keydown',
+                            //         function: this.StopchangeItem
+                            //     }
+                            // ]
                         )
                     }
                 })
@@ -68,17 +73,35 @@ export default class Inventory extends Event {
             Object.entries(data).map(entry => {
                 if ('weapons' == entry[0]) {
                     this.switchItem(data.weapons[1], data.weapons, 'weapons');
+                    this.changeItem(data.weapons[0], data.weapons)
                     // this.switchItem(data.weapons[0], data.weapons, 'weapons');
                 }
             })
         })
     }
 
-    changeItem(){
-        document.getElementById('inventoryOverlay').style.display = "block";
+
+    /**
+     * @param {string} newOld
+     * @param {object} element 
+     */
+    updateChangeItem(newOld, element) {
+        document.getElementById('inventoryOverlayImage').src = element.texture;
+        let stats = document.getElementById(`inventoryOverlayStats${newOld}`).childNodes;
+        stats[1].innerHTML = element.name;
+        stats[3].innerHTML = `${element.type}: ${element.value}`
+        stats[5].innerHTML = `Rarity: ${this.CalculateRarity(element.rarity)}`
+
     }
 
-    StopchangeItem(){
+    changeItem(oldItem, newItems) {
+        document.getElementById('inventoryOverlay').style.display = "flex";
+        let index = 0;
+        this.updateChangeItem('Old', newItems[0])
+        this.updateChangeItem('New', newItems[1])
+    }
+
+    StopchangeItem() {
         document.getElementById('inventoryOverlay').style.display = "none";
     }
 
@@ -90,5 +113,31 @@ export default class Inventory extends Event {
 
         element[0].innerHTML = `${newItem.name} - ${newItem.value} ${newItem.type}`;
         element[1].src = `${newItem.texture}`;
+    }
+
+    CalculateRarity(rarity) {
+        switch (rarity) {
+            case 1:
+                return 'common'
+            case 2:
+                return 'uncommon'
+            case 3:
+                return 'rare'
+            case 4:
+                return 'legendary'
+            case 5:
+                return 'exotic'
+            default:
+                return 'common'
+        }
+    }
+
+    //function calcalate difference return true or false
+    calculateDifference(itemA, itemB) {
+        if (itemA > itemB) {
+            return true;
+        } else if (itemA < itemB) {
+            return false;
+        }
     }
 }
